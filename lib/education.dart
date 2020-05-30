2d69c737cade7f05ee8208dafd91ac3b994c48fa
Ghaffaru <mudashiruagm@gmail.com>
2020-05-30 13:25:26 +0000

    file validations

--- lib/education.dart
+++ lib/education.dart
@@ -4,7 +4,11 @@ class Education extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: Text('Education'),
+      ),
+      backgroundColor: Colors.white,
       body: ListView(
         children: <Widget>[
           SizedBox(
@@ -12,36 +16,36 @@ class Education extends StatelessWidget {
           ),
           Row(
             children: <Widget>[
-              SizedBox(
-                width: 10,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/report');
-                },
-                alignment: Alignment.topLeft,
-                icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.white,
-                ),
-                iconSize: 50,
-                color: Colors.white,
-              ),
+//              SizedBox(
+//                width: 10,
+//              ),
+//              IconButton(
+//                onPressed: () {
+//                  Navigator.pushNamed(context, '/report');
+//                },
+//                alignment: Alignment.topLeft,
+//                icon: Icon(
+//                  Icons.arrow_back_ios,
+//                  color: Colors.white,
+//                ),
+//                iconSize: 50,
+//                color: Colors.white,
+//              ),
             ],
           ),
-          Text(
-            'EDUCATION PAGE',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
-          ),
+//          Text(
+//            'EDUCATION PAGE',
+//            textAlign: TextAlign.center,
+//            style: TextStyle(
+//                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+//          ),
           SizedBox(
             height: 10,
           ),
           Text(
             'This page will give you information on human trafficking safety tips',
             style: TextStyle(
-              color: Colors.white70,
+              color: Colors.blue,
               fontSize: 17,
             ),
             textAlign: TextAlign.center,
@@ -56,49 +60,49 @@ class Education extends StatelessWidget {
               children: <Widget>[
                 Text(
                   'Report any unusual activity to your local law enforcement agencies or uploadinformation on this portal.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'Do not engage in any conversation with anyone you are suspicious about.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'If anyone makes sexual advances at you, report the person to trusted people or to you local law enforcement agencies. Alternatively, you can describe the incident on this portal.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'Do not meet up with strangers alone in unusual places. Try to meet up in public place where you can visibly see people around you. It is advisable to go with a trusted person especially if you are a minor.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'Do not engage in any form of sexual conversation via the internet with anyone especially if they are strangers.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'If you are a minor (below 18) report all forms of abuse via the internet immediately to person you can trust. You can also report to your local law enforcement agencies Alternatively, describe the incident on this app.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
                   'If you notice any cases of abuse or you become a victim of abuse, do not be afraid to report the issue to a trusted person or your local law enforcement agency. Alternatively,describe the incident on this app.',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                 )
               ],
             ),
@@ -106,10 +110,22 @@ class Education extends StatelessWidget {
 
                 shape: BoxShape.rectangle, border: Border.all(width: 2, color: Colors.black),),
           ),
-          FlatButton(
-
-            child: Text('CONTACT US'),
-          )
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: Material(
+              elevation: 4.0,
+              color: Colors.orangeAccent,
+              borderRadius: BorderRadius.circular(20),
+              child: MaterialButton(
+                minWidth: 100,
+                height: 42,
+                child: Text(
+                  'CONTACT US',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/report.dart
+++ lib/report.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:mime_type/mime_type.dart';
+//
 //import 'package:flutter_uploader/flutter_uploader.dart';
 
 class Report extends StatefulWidget {
@@ -8,36 +13,110 @@ class Report extends StatefulWidget {
 
 class _ReportState extends State<Report> {
   final GlobalKey<FormState> formKey = GlobalKey();
+  String name;
+  String email;
+  String phoneNumber;
+  String location;
+  String description;
+  File file;
 
+  Future showUploadError() async {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('File Error'),
+            content: Text(
+              'Invalid file uploaded. Please Upload File or Video',
+              style: TextStyle(color: Colors.orangeAccent),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  Future showFileSizeError() async {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('File Error'),
+            content: Text(
+              'Huge file uploaded. Please Upload less than 100MB',
+              style: TextStyle(color: Colors.orangeAccent),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+  void onFileUploadPressed() async {
+    File file = await FilePicker.getFile();
+
+    if (file.existsSync()) {
+      String mimetype = mime(file.path);
+      print(mimetype);
+      List mimeArray = mimetype.split('/');
+      if (mimeArray[0] != 'image' &&
+          mimeArray[0] != 'video') {
+        showUploadError();
+        print('Invalid File Type, Please Upload an image or video');
+      } else if (file.lengthSync() > 100) {
+          showFileSizeError();
+      }
+
+    }
+  }
+
+  submitData() async {
+    onFileUploadPressed();
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: Text('Report Case'),
+      ),
+      backgroundColor: Colors.white,
       body: ListView(
         children: <Widget>[
-          SizedBox(
-            height: size.height * 0.04,
-          ),
-          Row(
-            children: <Widget>[
-              SizedBox(
-                width: 10,
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-                alignment: Alignment.topLeft,
-                icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.white,
-                ),
-                iconSize: 50,
-                color: Colors.white,
-              ),
-            ],
-          ),
+//          SizedBox(
+//            height: size.height * 0.04,
+//          ),
+//          Row(
+//            children: <Widget>[
+//              SizedBox(
+//                width: 10,
+//              ),
+//              IconButton(
+//                onPressed: () {
+//                  Navigator.pushNamed(context, '/');
+//                },
+//                alignment: Alignment.topLeft,
+//                icon: Icon(
+//                  Icons.arrow_back_ios,
+//                  color: Colors.white,
+//                ),
+//                iconSize: 50,
+//                color: Colors.white,
+//              ),
+//            ],
+//          ),
           Container(
               padding: EdgeInsets.all(20),
               child: Form(
@@ -45,42 +124,49 @@ class _ReportState extends State<Report> {
                 child: Column(
                   children: <Widget>[
                     TextFormField(
+                      style: TextStyle(color: Colors.blue),
                       decoration: InputDecoration(
+
                           labelText: 'Name (Optional)',
-                          labelStyle: TextStyle(color: Colors.white)),
+                          labelStyle: TextStyle(color: Colors.orangeAccent)),
                     ),
                     SizedBox(
                       height: size.height * 0.01,
                     ),
                     TextFormField(
+                      style: TextStyle(color: Colors.blue),
                       decoration: InputDecoration(
+
                           labelText: 'Email (Optional)',
-                          labelStyle: TextStyle(color: Colors.white)),
+                          labelStyle: TextStyle(color: Colors.orangeAccent)),
                     ),
                     SizedBox(
                       height: size.height * 0.01,
                     ),
                     TextFormField(
+                      style: TextStyle(color: Colors.blue),
                       decoration: InputDecoration(
                           labelText: 'Phone Number (Optional)',
-                          labelStyle: TextStyle(color: Colors.white)),
+                          labelStyle: TextStyle(color: Colors.orangeAccent)),
                     ),
                     SizedBox(
                       height: size.height * 0.01,
                     ),
                     TextFormField(
+                      style: TextStyle(color: Colors.blue),
                       decoration: InputDecoration(
                           labelText: 'Location (Optional)',
-                          labelStyle: TextStyle(color: Colors.white)),
+                          labelStyle: TextStyle(color: Colors.orangeAccent)),
                     ),
                     SizedBox(
                       height: size.height * 0.01,
                     ),
                     TextFormField(
+                      style: TextStyle(color: Colors.blue),
                       maxLines: 4,
                       decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          labelStyle: TextStyle(color: Colors.white),
+                          labelStyle: TextStyle(color: Colors.orangeAccent),
                           labelText: 'Description'),
                     ),
                     SizedBox(
@@ -88,29 +174,55 @@ class _ReportState extends State<Report> {
                     ),
 //                Text('Upload Image/Video', style: TextStyle(color: Colors.white),),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
-                        RaisedButton(
-                          color: Colors.white,
-                          child: Text(
-                            'Upload Image',
-                            style: TextStyle(color: Colors.white),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: Material(
+
+                            elevation: 2.0,
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            child: MaterialButton(
+                              onPressed: onFileUploadPressed,
+                              minWidth: 100,
+                              height: 10,
+                              child: Text(
+                                'Upload Image/Video',
+                                style: TextStyle(color: Colors.orangeAccent),
+                              ),
+                            ),
                           ),
                         ),
                         SizedBox(
                           width: 30,
                         ),
-                        RaisedButton(
-                          color: Colors.white,
-                          child: Text(
-                            'Upload Video',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        )
+//                        RaisedButton(
+//                          color: Colors.white,
+//                          child: Text(
+//                            'Upload Video',
+//                            style: TextStyle(color: Colors.white),
+//                          ),
+//                        )
                       ],
                     ),
-                    MaterialButton(
-                      child: Text('SUBMIT'),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Material(
+
+                        elevation: 4.0,
+                        color: Colors.orangeAccent,
+                        borderRadius: BorderRadius.circular(20),
+                        child: MaterialButton(
+                          onPressed: submitData,
+                          minWidth: 200,
+                          height: 42,
+                          child: Text(
+                            'SUBMIT',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
 
                     IconButton(
@@ -121,7 +233,7 @@ class _ReportState extends State<Report> {
                       alignment: Alignment.bottomRight,
                       icon: Icon(
                         Icons.arrow_forward_ios,
-                        color: Colors.white,
+                        color: Colors.orangeAccent,
                       ),
                       iconSize: 50,
                       color: Colors.white,
